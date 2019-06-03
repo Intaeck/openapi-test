50957e70f3f01db762fadbcf3d17c1b6e8e64dda
kb <kb@kbs-MacBook-Pro.local>
2019-06-03 10:40:48 +0900

    second

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 // body-parser를 이용해 application/x-www-form-urlencoded 파싱
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 // body-parser를 이용해 application/json 파싱
 app.use(bodyParser.json())
 
@@ -58,19 +57,21 @@ app.get('/test', (req, res) => {
         method:'POST',
         //url: 'https://dev-openapi.kbstar.com:8443/land/getOfferList/ver1',
         url: 'https://dev-openapi.kbstar.com:8443/caq/getLiivTalkTalkAccountStatus/ver1',
-        body: body,
+        body: jsondata,
         //json: true
         },
         function(error, response, body){
-            if(!error && response.statusCode==200) {
-                console.log(body);
-                response.on('data', (data)=> {
-                    console.log('data ===> ' ,data);
-                })
+            if(!error && response.statusCode == 200) {
+                console.log('JITKB ===> : ', body);
+                // response.on('data', (data)=> {
+                //     console.log('data ===> ' ,data);
+                // })
                 //res.send(body.statusCode)
                 res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-                res.write('<h1>hsKey 변환결과 입니다.</h1>');
-                res.write('<div><p>hsKey : ' + hsKey + '</p></div>');
+                res.write('<h1>hsKey 변환결과 입니다.</h1><br>');
+                res.write('<div><p>hsKey : ' + hsKey + '</p></div><hr>');                
+                res.write('<h1>OPEN API 응답결과 입니다.</h1><br>');
+                res.write('<div><p>' + body + '</p></div>')
                 //res.write('<div><p>' + body.bodyParser.data + '</p></div>')
                 res.write("<br><br>");
                 res.end();                
@@ -96,11 +97,16 @@ router.route('/trans').post(function(req, res) {
 
 	var paramId = req.body.apikey || req.query.apikey;
     var paramData = req.body.bodydata || req.query.bodydata;
- 
-    console.log('JSON data ===> : ', JSON.stringify(paramData))
-    var hmac = crypto.createHmac('sha256', paramId)
+
+    //JSON data 공백제거 
+    var bodydata = JSON.parse(String(paramData).replace(/\s/g,"").replace(/\n/g, "\\n").replace(/\r/g, "\\r"))
+    // bodydata = bodydata.replace(/\n/g,'')
+    // bodydata = bodydata.replace(/\r/g,'')
+    console.log('apikey ===> : ', String(paramId))
+    console.log('JSON data ===> : ', JSON.stringify(bodydata))
+    var hmac = crypto.createHmac('sha256', String(paramId))
     //var hmac = crypto.createHmac('sha256', new Buffer(appkey))
-    var encoded_hmac = hmac.update(JSON.stringify(paramData)).digest('base64')
+    var encoded_hmac = hmac.update(JSON.stringify(bodydata)).digest('base64')
     
 	res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 	res.write('<h1>hsKey 변환결과 입니다.</h1>');
